904fc95d40a5d55019fcdd035f9cdea83811b06e
nam <nam.pv1805@gmail.com>
2022-09-23 16:19:46 +0700

    learn new things

--- contracts/test-ownership/src/main.rs
+++ contracts/test-ownership/src/main.rs
@@ -1,15 +1,24 @@
-
-fn do_something(vec: &mut Vec<i32>){
-    vec.push(4);
-    println!("{:?}", vec);
-    println!("Inside doSomething()");
-}
+#![allow(unused)]
 
 fn main() {
-    let mut vec = Vec::new();
-    vec.push(1);
-    vec.push(2);
-    do_something(&mut vec);
-    vec.push(3);
-    println!("{:?}", vec);
+    let mut s: String = String::from("Hello");
+    s.push_str(", World!");
+    println!("{}", s);
+
+    // let mut a: &str = "Hi";
+    // println!("{}", a);
+    // a = "Hello";
+    // println!("{}", a);
+
+    let mut s1: String = String::from("Hello ");
+    let mut s2 = &mut s1;
+    s2.push_str("string");
+    println!("{:p}", s2);
+    s1.push_str("vkl");
+    println!("{:p}", &s1);
+
+    // s1.push_str("string");    
+    // println!("{}", s1);
+    // s2.push_str("How");
+    // println!("{}", s2);
 }
